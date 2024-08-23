6ba1bd6a8f789658a38d9feb08714fe143b8642f
Kairi0913 <yotakarasu.gsx60r@gmail.com>
2024-08-24 00:45:33 +0900

    判定まで完成！

--- zoiac/js/zodiac.js
+++ zoiac/js/zodiac.js
@@ -1,54 +1,47 @@
-//https://zenn.dev/okoe/articles/7876b897c0fccf
+document.getElementById("send").onclick = function() {
+    let name = document.getElementById("name");
+    let month = document.getElementById("selectMonth");
+    let day = document.getElementById("selectDay");
 
-let userBirthdayYear = document.querySelector(".birthday-year");
-let userBirthdayMonth = document.querySelector(".birthday-month");
-let userBirthdayDay = document.querySelector(".birthday-day");
+    console.log(name.value);
+    console.log(month.value);
+    console.log(day.value);
 
-function createOptionForElements(elem, val) {
-    let option = document.createElement("option");
-    option.text = val;
-    option.value = val;
-    elem.appendChild(option);
+    zodiac(month.value, day.value);
 }
 
-for(let i = 1920; i <= 2020; i++) {
-    createOptionForElements(userBirthdayYear, i);
-}
-for(let i = 1; i <= 12; i++) {
-    createOptionForElements(userBirthdayMonth, i);
-}
-for(let i = 1; i <= 31; i++) {
-    createOptionForElements(userBirthdayDay, i);
-}
+function zodiac (month, day) {
+    // let zodiac = seiza;
 
-function changeTheDay() {
+    // zodiacMonth = parseInt(month);
+    // zodiacDay = parseInt(day);
 
-    userBirthdayDay.innerHTML = "";
+    zodiacDay = month + day;
+    console.log(zodiacDay);
 
-    let lastDayOfTheMonth = new Date(userBirthdayYear.value, userBirthdayMonth.value, 0).getDate();
-
-    for(let i = 1; i <= lastDayOfTheMonth; i++) {
-        createOptionForElements(userBirthdayDay, i);
+    if (zodiacDay >= 120 && zodiacDay <= 218) {
+        console.log("水瓶座（みずがめ座）");
+    } else if (zodiacDay >= 219 && zodiacDay <= 320) {
+        console.log("魚座（うお座）");
+    } else if (zodiacDay >= 321 && zodiacDay <= 419) {
+        console.log("牡羊座（おひつじ座）です。");
+    } else if (zodiacDay >= 420 && zodiacDay <= 520) {
+        console.log("牡牛座（おうし座）です。");
+    } else if (zodiacDay >= 521 && zodiacDay <= 621) {
+        console.log("双子座（ふたご座）です。");
+    } else if (zodiacDay >= 622 && zodiacDay <= 722) {
+        console.log("蟹座（かに座）です。");
+    } else if (zodiacDay >= 723 && zodiacDay <= 822) {
+        console.log("獅子座（しし座）です。");
+    } else if (zodiacDay >= 823 && zodiacDay <= 922) {
+        console.log("乙女座（おとめ座）です。");
+    } else if (zodiacDay >= 923 && zodiacDay <= 1023) {
+        console.log("天秤座（てんびん座）です。");
+    } else if (zodiacDay >= 1024 && zodiacDay <= 1122) {
+        console.log("蠍座（さそり座）です。");
+    } else if (zodiacDay >= 1123 && zodiacDay <= 1221) {
+        console.log("射手座（いて座）です。");
+    } else {
+        console.log("山羊座（やぎ座）です。");
     }
 }
-
-userBirthdayYear.addEventListener("change", function() {
-    changeTheDay();
-});
-
-userBirthdayMonth.addEventListener("change", function() {
-    changeTheDay();
-});
-
-
-
-
-
-
-// document.getElementById("send").onclick = function() {
-//     let name = document.getElementById("name").value;
-//     let date = document.getElementById("date").value;
-
-//     console.log(name);
-//     console.log(date);
-// }
